18db1fa1fbcd4933a2d336b1fd3700c69239ad96
Uriel Cabrera <59104218+uricabrera@users.noreply.github.com>
2024-11-04 20:06:42 -0300

    Modificar Routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { register, login, recoverPassword, resetPassword } = require('../controllers/auth');
+const { authenticateToken, verifyRecoveryToken } = require('../middleware/auth'); // Asegúrate de importar ambas funciones
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/recovery', recoverPassword);
 router.post('/reset/:token', resetPassword);
+
+// Ruta para verificar el token de recuperación
+router.post('/verify-token', verifyRecoveryToken, (req, res) => {
+    res.json({ message: 'Recovery token is valid', user: req.user });
+});
 
 module.exports = router;
