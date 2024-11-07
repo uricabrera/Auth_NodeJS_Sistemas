c2033dc53be248b5c33ee29f652928eeacb78dbe
Uriel Cabrera <59104218+uricabrera@users.noreply.github.com>
2024-11-07 19:22:09 -0300

    Test db.js

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: "authapidb01.mysql.database.azure.com",
+    user: "adminsistemas",
+    password: "TestApiAuth123",
+    database: "auth_db",
+    port: 3306
 });
 
 connection.connect((err) => {
